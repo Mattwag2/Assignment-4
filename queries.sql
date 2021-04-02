1a7a16c5b19b09af6ee1b311e7d73e426114e2f1
mattwag2 <mr.matt.wagner@gmail.com>
2021-04-01 19:06:26 -0500

    Complete?

--- queries.sql
+++ queries.sql
@@ -1,7 +1,19 @@
 ## Part 1: Test it with SQL
 
+SELECT * FROM techjobs.jobs;
+
 ## Part 2: Test it with SQL
+
+SELECT name FROM techjobs.employer WHERE location is "St. Louis City";
 
 ## Part 3: Test it with SQL
 
+DROP TABLE if exists job;
+
 ## Part 4: Test it with SQL
+
+select name, description
+from skill
+inner join job_skills on  skill.id = job_skills.skills_id
+where job_skills.jobs_id is not null
+order by name asc;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/EmployerController.java
@@ -1,17 +1,25 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
 import org.launchcode.javawebdevtechjobspersistent.models.Employer;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
 
 
     @GetMapping("add")
@@ -21,20 +29,21 @@ public class EmployerController {
     }
 
     @PostMapping("add")
-    public String processAddEmployerForm(@ModelAttribute @Valid Employer newEmployer,
+    public String processAddEmployerForm(@ModelAttribute @Valid Employer employer,
                                     Errors errors, Model model) {
 
         if (errors.hasErrors()) {
             return "employers/add";
         }
 
+        employerRepository.save(employer);
         return "redirect:";
     }
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
@@ -43,4 +52,14 @@ public class EmployerController {
             return "redirect:../";
         }
     }
+
+
+    @GetMapping("")
+    public String employerIndex (Model model) {
+
+
+        model.addAttribute("employers", employerRepository.findAll());
+        return "employers/index";
+    }   
+
 }
